cca0762cf142c2eba2b8f180a59566db7d0abf85
Sai Krishna <yachamsaikrishna2006@gmail.com>
2026-06-18 00:17:32 +0530

    Added Streamlit secrets support

--- utils/rag_engine.py
+++ utils/rag_engine.py
@@ -1,12 +1,16 @@
 import os
+import streamlit as st
 import google.generativeai as genai
 from dotenv import load_dotenv
 
 load_dotenv()
 
-genai.configure(
-    api_key=os.getenv("GEMINI_API_KEY")
-)
+api_key = os.getenv("GEMINI_API_KEY")
+
+if not api_key:
+    api_key = st.secrets["GEMINI_API_KEY"]
+
+genai.configure(api_key=api_key)
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
